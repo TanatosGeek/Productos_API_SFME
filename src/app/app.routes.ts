a72349568b35a38d63068a99fe33c2f84f4db9d3
TanatosGeek <21160787@itoaxaca.edu.mx>
2024-12-01 20:38:11 -0600

    .

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'prefix' }, // Cambiado de 'full' a 'prefix'
   { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }, // Redirige cualquier ruta desconocida a login
 ];
 
 export const appRouterProviders = [provideRouter(routes)];
